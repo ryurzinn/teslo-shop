902dc10074b4d53957865cf5540fa900a46c6edc
ryurzinn <chinwenwencha47@gmail.com>
2025-08-14 10:17:16 -0300

    Ya logre insertar en la tabla

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,46 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Product {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: number;
+
+    @Column('text', {
+        unique: true,
+    })
+    title: string;
+
+    @Column('float', {
+        default: 0
+    })
+    price: number;
+
+    @Column({
+        type: 'text',
+        nullable: true
+    })
+    description: string;
+
+    @Column('text', {
+        unique: true
+    })
+    slug: string;
+
+    @Column('integer', {
+        default: 0
+    })
+    stock: number;
+
+    @Column('text', {
+        array: true
+    })
+    sizes: string[];
+
+    @Column('text')
+    gender: string;
+
+    //  tags
+    //  images
+
+}
