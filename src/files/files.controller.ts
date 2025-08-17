f3b3bf919636aa2124a34cefdc10fe9515a70a18
ryurzinn <chinwenwencha47@gmail.com>
2025-08-17 15:36:16 -0300

    retornar secureUrl

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,13 +1,28 @@
-import { BadRequestException, Controller, Post, UploadedFile, UseInterceptors,} from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param, Post, Res, UploadedFile, UseInterceptors,} from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter, fileNamer } from './helpers';
 import { diskStorage } from 'multer';
+import type { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService 
+  ) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response, // Emitir la respuesta manualmente 
+    @Param('imageName') imageName: string
+  ) {
+    const path = this.filesService.getStaticProductImage( imageName ); // Este es el path
+
+    res.sendFile( path );
+  }
 
   @Post('product')
   @UseInterceptors( FileInterceptor('file', {
@@ -24,9 +39,10 @@ export class FilesController {
         throw new BadRequestException('Make sure that the file is an image');
       }
 
+      // const secureUrl = `${file.filename}`
+      const secureUrl = `${ this.configService.get('HOST_API') }/files/product/${ file.filename }`;
 
-      return {
-        fileName: file.originalname
-      };
+
+      return {secureUrl};
     }
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,6 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { existsSync } from 'fs';
+import { join } from 'path';
 
 @Injectable()
 export class FilesService {
  
+    getStaticProductImage( imageName: string)  { // Da igual el tipo de archivo esto verifica que el file exista sin importar el tipo de dato
+
+        const path = join( __dirname, '../../static/products', imageName);
+
+        if( !existsSync(path) ) // Si el path no existe
+            throw new BadRequestException(`No product found with image ${imageName}`);// Devuelve una BadRequest
+
+        return path; // Si existe retorna el path
+
+    }
 }
