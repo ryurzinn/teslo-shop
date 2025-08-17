cefeb0bd50446cc025b17a2f971a41b36d3a3181
ryurzinn <chinwenwencha47@gmail.com>
2025-08-17 14:49:53 -0300

    Recupere el files.controller que se habia borrado

---
+++ src/files/files.controller.ts
@@ -0,0 +1,32 @@
+import { BadRequestException, Controller, Post, UploadedFile, UseInterceptors,} from '@nestjs/common';
+import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter, fileNamer } from './helpers';
+import { diskStorage } from 'multer';
+
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('product')
+  @UseInterceptors( FileInterceptor('file', {
+    fileFilter: fileFilter,
+    // limits: {fileSize: 1000}
+    storage: diskStorage({
+      destination: './static/products',
+      filename: fileNamer
+    })
+  }) )
+    uploadProductImage( @UploadedFile() file: Express.Multer.File){
+
+      if( !file  ) {
+        throw new BadRequestException('Make sure that the file is an image');
+      }
+
+
+      return {
+        fileName: file.originalname
+      };
+    }
+}
